95a693deb735e485ccbf723a074ed07068b3e857
jhekasoft <jheka@mail.ru>
2013-01-20 01:22:35 +0200

    Fixed bug on change screen orientation

--- src/jhekasoft/lotto/AboutActivity.java
+++ src/jhekasoft/lotto/AboutActivity.java
@@ -2,6 +2,8 @@ package jhekasoft.lotto;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 /**
  *
@@ -15,6 +17,12 @@ public class AboutActivity extends Activity {
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        setContentView(R.layout.about);       
+        setContentView(R.layout.about);
+        
+//        // Make link active
+//        TextView textViewAbout = (TextView) findViewById(R.id.textViewAbout);
+//        if (textViewAbout != null) {
+//            textViewAbout.setMovementMethod(LinkMovementMethod.getInstance());
+//        }
     }
 }
